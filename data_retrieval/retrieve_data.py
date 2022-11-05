22bf35fe60774b8882afe54bc501be922b7f9ed6
Mike Elzas <mikeelzas@gmail.com>
2022-11-05 12:44:13 +0100

    first version of data retrieval, where we now obtain CSV's for different tickers

--- data_retrieval/retrieve_data.py
+++ data_retrieval/retrieve_data.py
@@ -1,36 +1,34 @@
-
-from datetime import datetime, timedelta
+import ccxt
 import pandas as pd
+import os
 
-
-
-def retrieve_data(days, ticker, timeframe):
+def retrieve_data():
     """
     this function is used to retrieve data from FTX.
     days = the amount of days the user would like to look back.
     ticker = the ticker the user wants to retrieve data on e.g. BTC/USD.
     timeframe = how often the data should get retrieved e.g. 1m, 1h.
     """
-    import ccxt
 
-    #here the timedelta is calculated based on the number of days
-    since = datetime.utcnow() - timedelta(days=days)
-    since_ms = int(since.timestamp()) * 1000
+    columns = ["time", "open", "high", "low", "close", "volume"]
+    ticker_list = ["BTC/USDT", "ETH/USDT", "SOL/USDT"]
+    work_dir = os.path.abspath("..")+ "/data/ticker_data/"
+
 
     #retrieve data from the exchange
-    ftx = ccxt.ftx()
-    data = ftx.fetch_ohlcv(symbol = ticker,timeframe = timeframe, since = since_ms )
+    for ticker in ticker_list:
+        ftx = ccxt.ftx()
+        from_ts = ftx.parse8601('2021-01-01 00:00:00')
+        ohlcv = ftx.fetch_ohlcv(ticker, '1h', since=from_ts, limit=1000)
+        ohlcv_list = ohlcv.copy()
+        #if the len of OHLCV is 1000 it indicates that there is still more data left, if it is less then we know we are at the end
+        while(len(ohlcv)==1000):
+            from_ts = ohlcv[-1][0]
+            ohlcv.clear()
+            ohlcv = ftx.fetch_ohlcv(ticker, '1h', since=from_ts, limit=1000)
+            ohlcv_list.extend(ohlcv)
 
-    #convert the data to a DataFrame
-    ohlcv = pd.DataFrame(data, columns = ["time", "open", "high", "low", "close", "volume"])
-
-    #converting the time column to a proper date format
-    ohlcv['time'] = ohlcv['time'].apply(lambda x : datetime.fromtimestamp(x/1000).strftime('%Y-%m-%d %H:%M'))
-
-
-    #eventually we use this function so the user can fill in a date format and it gets converted
-    #import time, datetime
-    #print(time.mktime(datetime.datetime.strptime(string,"%d/%m/%Y").timetuple()))
-
-
-    return ohlcv
+    #convert the data to a DataFrame and write it to a local csv file in the ticker_data folder
+        ohlcv = pd.DataFrame(ohlcv_list, columns=columns)
+        ticker = ticker.replace("/", "_")
+        ohlcv.to_csv(f"{work_dir}{ticker}", header=columns)
